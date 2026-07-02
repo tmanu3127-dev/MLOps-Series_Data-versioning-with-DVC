8b91368f46e2a712149c53e5b69828cdc0028526
Manpreet <tmanu3127@gmail.com>
2026-07-03 01:47:53 +0530

    second version of data saved

--- mycode.py
+++ mycode.py
@@ -11,12 +11,17 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 df = pd.DataFrame(data)
 
 
-# Adding new row to df for v2
-# new_row_loc2 = {'Name': 'V2', 'Age': 20, 'City': 'Miami'}
+# Adding new row to df for v2  ;these we will perform after some time to prove
+#  that what we changed will store in the dvc 
+new_row_loc2 = {'Name': 'V2', 'Age': 20, 'City': 'Miami'}
 
-# df.loc[len(df.index)] = new_row_loc2
+df.loc[len(df.index)] = new_row_loc2
 
-# Ensure the data directory exsits at the root level:
+# Adding new row to df for v3
+# new_row_loc3 = {'Name': 'V3', 'Age': 30, 'City': 'City1'}
+# df.loc[len(df.index)] = new_row_loc3
+
+# Ensure the data directory exsits at the root level of the project, if not create it:
 
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
